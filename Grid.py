47bba7a69b7b7a978fdccd1dc80385704cf681d6
unknown <nkhbes001@myuct.ac.za>
2023-08-15 21:49:22 +0200

    interactions

--- Grid.py
+++ Grid.py
@@ -8,27 +8,32 @@ class GridApp:
         self.cols = cols
         
         self.create_grid()
-        self.place_point()
+        #self.place_point()
+
 
     def create_grid(self):
         self.cells = [[None] * self.cols for _ in range(self.rows)]
         
         for i in range(self.rows):
             for j in range(self.cols):
-                cell_frame = tk.Frame(self.root, width=50, height=30, highlightthickness=1, highlightbackground="black")
+                cell_frame = tk.Frame(self.root, width=50, height=50, highlightthickness=1, highlightbackground="black")
                 cell_frame.grid(row=i, column=j)
                 
                 label = tk.Label(cell_frame, text="", width=6, height=2)
                 label.pack(fill=tk.BOTH, expand=True)
                 
+                
+                cell_frame.config(bg="blue")
+                
                 self.cells[i][j] = label
                 
-    def place_point(self):
-        row = random.randint(0, self.rows - 1)
-        col = random.randint(0, self.cols - 1)
+    def place_point(self, row , col):
+        #row = random.randint(0, self.rows - 1)
+        #col = random.randint(0, self.cols - 1)
         
         point_label = self.cells[row][col]
-        point_label.config(text="*", font=("Helvetica", 14))
+        point_label.config(text="((;))", font=("Helvetica", 14))
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -38,5 +43,9 @@ if __name__ == "__main__":
     num_cols = 10  # Adjust the number of columns
     
     app = GridApp(root, num_rows, num_cols)
+    row = int(input("Which row do you wanna place a point : "))
+    col = int(input("Which collumn do you wanna place a point: "))
+    
+    app.place_point(row, col)
     
     root.mainloop()
